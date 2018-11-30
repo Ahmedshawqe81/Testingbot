d76881b028576c6afe2553c8e860aaa1eb5b8f35
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2018-11-30 20:55:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,8 @@ client.on('message', message => {
  var prefix = "!"
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لآ يوجد رتبه بـ اسم "Support Team " 
+قم بأضافه الرتبه و جرب مره اخري`);
     if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`يوجد بالفعل تكت مفتوح.`);
     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
         let role = message.guild.roles.find("name", "Support Team");
